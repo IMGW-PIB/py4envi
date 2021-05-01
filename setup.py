31112fba498b3bd78659d753d30b611e73f7141e
Konrad Malik <konrad.malik@gmail.com>
2021-05-01 20:22:51 +0200

    remove python 3.6

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     license="LGPL-2.1",
     description="Library/cli tool for https://dane.sat4envi.imgw.pl/",
     url="https://github.com/IMGW-PIB/py4envi",
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=REQUIRES,
     packages=find_packages(exclude=["test", "tests"]),
     include_package_data=True,
@@ -36,7 +36,6 @@ setup(
     classifiers=[
         "License :: OSI Approved :: LGPL-2.1 License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
