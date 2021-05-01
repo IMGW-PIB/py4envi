ee8a255a22e5442f8a7a97c4ba691ef8b0df7c2c
Konrad Malik <konrad.malik@gmail.com>
2021-05-01 20:21:09 +0200

    improve setup for pypi and support python 3.6

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import pathlib
 from setuptools import setup, find_packages
 from py4envi import __version__
 
@@ -11,22 +12,33 @@ REQUIRES = [
     "tqdm >= 4.60.0",
 ]
 
+HERE = pathlib.Path(__file__).parent
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
 setup(
     name=NAME,
     version=__version__,
     author="Konrad Malik",
-    author_email="konrad.malik@imgw.pl",
+    author_email="konrad.malik@gmail.com",
+    license="LGPL-2.1",
     description="Library/cli tool for https://dane.sat4envi.imgw.pl/",
-    url="https://dane.sat4envi.imgw.pl/",
-    keywords=["sat4envi", "api", "sentinel", "esa", "imgw"],
+    url="https://github.com/IMGW-PIB/py4envi",
     python_requires=">=3.6",
     install_requires=REQUIRES,
     packages=find_packages(exclude=["test", "tests"]),
     include_package_data=True,
-    long_description="""\
-        Library/cli tool for https://dane.sat4envi.imgw.pl/
-    """,
+    long_description=README,
+    long_description_content_type="text/markdown",
     entry_points={
         "console_scripts": ["py4envi = py4envi.__main__:main"],
     },
+    classifiers=[
+        "License :: OSI Approved :: LGPL-2.1 License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
